2ee9cce1e2fdb3616070510af2809b0bd392c646
Antonytm <antontishchenko@gmail.com>
2022-02-09 08:08:02 +0200

    WiP

---
+++ next-js-typescript/components/SixtySeconds.js
@@ -0,0 +1,26 @@
+function SixtySeconds({ date }) {
+    return (
+        <h3>
+            {date}
+        </h3>
+    )
+}
+
+// This function gets called at build time on server-side.
+// It may be called again, on a serverless function, if
+// revalidation is enabled and a new request comes in
+export async function getStaticProps() {
+    const date = new Date().toLocaleTimeString() + " each 60 seconds. ";
+    return {
+        props: {
+            date,
+        },
+        // Next.js will attempt to re-generate the page:
+        // - When a request comes in
+        // - At most once every 10 seconds
+        revalidate: 60, // In seconds
+    }
+}
+
+
+export default SixtySeconds;
